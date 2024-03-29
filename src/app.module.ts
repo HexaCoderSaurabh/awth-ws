28730e940e186270608ec44da7d6ecb6c33504f1
saurabh-indianihub <2021.saurabh.agrawal@gmail.com>
2024-03-29 22:39:22 +0530

    feat: Adding User Controller and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigValidationSchema } from './config.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: ConfigValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          dialectOptions: {
+            decimalNumbers: true,
+          },
+          logging: true,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true
+        };
+      },
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
