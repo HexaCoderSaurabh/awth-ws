7a3f528197ce01fd661bbaaedb4e75ac3fd92d8a
saurabh-indianihub <2021.saurabh.agrawal@gmail.com>
2024-03-30 07:31:28 +0530

    feat: changed schema

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,16 +1,40 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
 @Entity()
+@Unique(['username', 'email', 'password'])
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  @Column()
-  firstName: string;
+  @Column({ length: 50, nullable: false })
+  username: string;
 
-  @Column()
-  lastName: string;
+  @Column({ length: 100, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ length: 50, nullable: false })
+  first_name: string;
+
+  @Column({ length: 50, nullable: false })
+  last_name: string;
 
   @Column({ default: true })
-  isActive: boolean;
+  is_active: boolean;
+
+  @Column({ default: 0 })
+  failed_attempts: number;
+
+  @Column({ default: 0 })
+  login_count: number;
+
+  @Column({ default: false })
+  email_verified: boolean;
+
+  @Column({ length: 100, nullable: true })
+  email_verification_token: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  email_verification_token_expiry: Date;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('');
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   await app.listen(port || 4000);
+  logger.log(`App running in port ${port}`)
 }
 bootstrap();
